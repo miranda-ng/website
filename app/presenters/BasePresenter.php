9834cab479d090761078900e45bafb82dd2aa2e2
Robyer <robyer@seznam.cz>
2013-12-15 12:18:02 +0100

    Multilanguage support, added gettexttranslator

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,25 +2,33 @@
 
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
-	/** @var bool */
-	public $oldLayoutMode = false;
+	/** @persistent */
+	public $lang;
 
+    /** @var GettextTranslator\Gettext @inject */
+    public $translator;
 
     public function  startup() {
         parent::startup();
-		
+
     }
 
     public function beforeRender()
     {
         parent::beforeRender();
-		
+
 		$year = $this->context->parameters["year"];
 		$this->template->copy = $year . (date("Y") > $year ? " - " . date("Y") : "");
-		
+
 		$this->template->sufix = $this->context->parameters["title"];
-		
-		$this->template->news_panel = $this->context->database->table("news")->order("date DESC")->limit(3);		
+
+		$this->template->news_panel = $this->context->database->table("news")->order("date DESC")->limit(3);
+
+		$this->template->langs = array(
+			"en" => "English",
+			"cs" => "Czech",
+			"ru" => "Russian",
+		);
 
 		$this->template->menu = array(
 			"Home:" => "Home",
@@ -33,51 +41,61 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 			//"Forums:" => "Forums",
 			"//forum.miranda-ng.org/" => "Forum",
 		);
-		
+
 		if ($this->user->isLoggedIn()) {
 			$this->template->menu["Admin:Home:"] = "Admin";
 		}
-		
+
 		$quotes = array();
 		$quotes[] = "„Do you know that <strong>Miranda NG</strong><br>is smaller, faster and easier?“";
 		//$quotes[] = "„Sun takes no prisoners!“";
 		$quotes[] = "„Miranda NG is better than sex!“<br><b>Satisfied user</b>";
 		$quotes[] = "„I don't always use Instant messengers...<br>But when I do, I use <strong>Miranda NG</strong>!";
-		
+
 		$this->template->quote = $quotes[array_rand($quotes)];
     }
-	
+
 	/**
 	 * Registers own template helpers.
 	 * @param type $class
-	 * @return type 
+	 * @return type
 	 */
 	protected function createTemplate($class = NULL) {
 		$template = parent::createTemplate($class);
+
+		// if not set, the default language will be used
+        if (!isset($this->lang)) {
+            $this->lang = $this->translator->getLang();
+        } else {
+            $this->translator->setLang($this->lang);
+        }
+
+        $template->setTranslator($this->translator);
+
 		return \Macros::setupTemplate($template);
-	}	
-	
+	}
+
 	/**
 	 * Registers own template macros.
-	 * @param type $template 
+	 * @param type $template
 	 */
 	public function templatePrepareFilters($template)
 	{
 		$template->registerFilter($latte = new \Nette\Latte\Engine);
 		\Macros::setupMacros($latte->compiler);
 	}
-		
+
 	protected function makeDescription($content) {
 		if (!preg_match('#<p ?.*?>(.*?)</p>#ims', $content, $match))
-			return NULL;		
-		
+			return NULL;
+
 		$str = trim(strip_tags($match[1]));
 		if (mb_strlen($str, "utf8") < 50)
-			return NULL;		
-		
+			return NULL;
+
 		return \Nette\Utils\Strings::truncate($str, 255);
 	}
-	
+
 	/**
 	 * Texyla loader factory
 	 * @return TexylaLoader
@@ -98,5 +116,5 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 		$texyla = new TexylaLoader($filter, $baseUri."webtemp", $this->context->parameters["wwwDir"]);
 		return $texyla;
 	}
-	
+
 }
--- app/services/routers/RouterFactory.php
+++ app/services/routers/RouterFactory.php
@@ -12,22 +12,25 @@ class RouterFactory
 	public function create()
 	{
 		$router = new RouteList();
-		
+
 		// Admin routers
 		$adminRouter = new RouteList('Admin');
 		$adminRouter[] = new Route('admin', array(
 				'presenter' => "Home",
 				'action' => "default",
 			), Route::ONE_WAY);
-		$adminRouter[] = new Route('/admin/<presenter>/<action>[/<id>]', 'Home:default');
+		$adminRouter[] = new Route('/admin/<lang>/<presenter>/<action>[/<id>]', 'Home:default');
 		$router[] = $adminRouter;
 
 		// Front routers
 		$router[] = new Route('index.php', 'Home:default', Route::ONE_WAY);
 
-		$router[] = new Route('news/<link>', array(
-				'presenter' => 'News',
-				'action' => 'show',
+		$lang = "[<lang [a-z]{2}>/]";
+
+		$router[] = new Route($lang . 'news/<link>', array(
+			'presenter' => 'News',
+			'action' => 'show',
+			'lang' => "en"
 		));
 
 		$router[] = new Route('p/<id>', array(
@@ -35,18 +38,20 @@ class RouterFactory
 			'action' => 'plugin',
 		));
 
-		$router[] = new Route('<presenter>[/<action>]/page/<vp-page>', array(
+		$router[] = new Route($lang . '<presenter>[/<action>]/page/<vp-page>', array(
 				'presenter' => 'Home',
 				'action' => 'default',
-				'id' => NULL
+				'id' => NULL,
+				'lang' => "en"
 		));
 
-		$router[] = new Route('<presenter>/<action>/<id>', array(
+		$router[] = new Route($lang . '<presenter>/<action>/<id>', array(
 				'presenter' => 'Home',
 				'action' => 'default',
-				'id' => NULL
+				'id' => NULL,
+				'lang' => "en"
 		));
-		
+
 		return $router;
 	}
 
