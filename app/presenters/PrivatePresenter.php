2c96285a5016a136745ca80e747af0f78729b66f
Robyer <robyer@seznam.cz>
2014-09-25 09:33:12 +0200

    Fix PrivatePresenter

--- app/presenters/PrivatePresenter.php
+++ app/presenters/PrivatePresenter.php
@@ -1,6 +1,6 @@
 <?php
 
-final class AddonsPresenter extends BasePresenter
+final class PrivatePresenter extends BasePresenter
 {
 
 	/** @var \Models\CategoriesModel @inject */
@@ -9,6 +9,8 @@ final class AddonsPresenter extends BasePresenter
 	/** @var \Models\AddonsModel @inject */
 	public $addonsModel;
 
+	private $wwwDir;
+
 	public function startup()
 	{
 		parent::startup();
@@ -16,6 +18,8 @@ final class AddonsPresenter extends BasePresenter
 		if ($this->getParameter("id") !== "key123ng") {
 			$this->error();
 		}
+
+		$this->wwwDir = $this->context->getParameters()["wwwDir"];
 	}
 
 	private function getRealFilename($headers, $url)
@@ -33,50 +37,102 @@ final class AddonsPresenter extends BasePresenter
 		return basename($stripped_url);
 	}
 
+	public function actionCheckFiles()
+	{
+		ob_end_flush();
+
+		/*$i = 0;
+		$items = $this->context->database->table("addons");
+		foreach ($items as $item) {
+
+			set_time_limit(30);
+
+			$filename = implode(DIRECTORY_SEPARATOR, [ $this->wwwDir, "upload", "files", $item->filename ]);
+
+			if (!file_exists($filename)) {
+				echo "Invalid: #$item->id - $item->filename<br>";
+			}
+
+			if (($i = ($i + 1) % 20) == 0)
+				flush();
+		}*/
+
+		$this->terminate();
+	}
+
+	public function actionRenameFiles()
+	{
+		ob_end_flush();
+
+		/*$i = 0;
+		$items = $this->context->database->table("addons")->where("renamed", 0);
+		foreach ($items as $item) {
+
+			set_time_limit(30);
+
+			$oldname = implode(DIRECTORY_SEPARATOR, [ $this->wwwDir, "upload", "files", $item->filename ]);
+			$newname = implode(DIRECTORY_SEPARATOR, [ $this->wwwDir, "upload", "files", $item->id . "_" . $item->filename ]);
+
+			if (@rename($oldname, $newname)) {
+				echo "Renamed: #$item->id - $item->filename<br>";
+				$item->update([
+					"renamed" => 1,
+				]);
+			}
+
+			if (($i = ($i + 1) % 20) == 0)
+				flush();
+		}*/
+
+		$this->terminate();
+	}
+
 	public function actionDownloadFiles()
 	{
 		ob_end_flush();
 
 		/*$i = 0;
+		$type = "source"; // "file";
 
 		//$items = $this->context->database->table("addons")->where("filename IS NULL");
-		//$items = $this->context->database->table("addons")->where("source", 1)->where("source_filename IS NULL");
-		$items = $this->context->database->table("addons")->where("source_filename IS NOT NULL")->where("source_version = ''");
+		$items = $this->context->database->table("addons")->where("downloaded", 0)->where("source_filename IS NOT NULL");
+		//$items = $this->context->database->table("addons")->where("downloaded", 0);
+
 		foreach ($items as $item) {
 
 			set_time_limit(30);
 
-			$url = 'http://addons.miranda-im.org/details.php?action=viewfile&id=' . $item->id;
-			//$url = 'http://addons.miranda-im.org/download.php?dlfile=' . $item->id;
-			//$url = 'http://addons.miranda-im.org/download.php?dlsource=' . $item->id;
-			echo "Downloading: $item->name - $url\n<br>";
+			//$url = 'http://addons.miranda-im.org/details.php?action=viewfile&id=' . $item->addons_id;
+			if ($type == "source") {
+				$url = 'http://addons.miranda-im.org/download.php?dlsource=' . $item->addons_id;
+			} else {
+				$url = 'http://addons.miranda-im.org/download.php?dlfile=' . $item->addons_id;
+			}
 
-			if (($i = ($i + 1) % 5) == 0)
+			echo "Downloading $type: $item->name - $url\n<br>";
+
+			if (($i = ($i + 1) % 10) == 0)
 				flush();
 
 			$content = file_get_contents($url);
 
-			preg_match('|<h3 class="border2" width=100%>Source Code</h3>.*?"indentText">(.*?)\s*-.*?</div>|is', $content, $matches);
-			$item->update(array("source_version" => $matches[1]));
+			//preg_match('|<h3 class="border2" width=100%>Source Code</h3>.*?"indentText">(.*?)\s*-.*?</div>|is', $content, $matches);
+			//$item->update(array("source_version" => $matches[1]));
 
+			$filename = $this->getRealFilename($http_response_header, $url); // http_response_header is filled automatically
 
-			//$filename = $this->getRealFilename($http_response_header, $url); // http_response_header is filled automatically
+			$path = implode(DIRECTORY_SEPARATOR, array($this->wwwDir, "upload", $type == "source" ? "sources" : "files", $item->id . "_" . $filename));
 
-			/*$params = $this->context->getParameters();
-			$wwwDir = $params["wwwDir"];
-
-			$f = fopen(implode(DIRECTORY_SEPARATOR, array($wwwDir, "upload", "files", $filename)), "w");
-			//$f = fopen(implode(DIRECTORY_SEPARATOR, array($wwwDir, "upload", "sources", $filename)), "w");
-
+			$f = fopen($path, "w");
 			if ($f) {
 				fwrite($f, $content);
 				fclose($f);
-			}*/
+			}
 
-			//$item->update(array("filename" => $filename));
-			//$item->update(array("source_filename" => $filename));
-
-		//}
+			$item->update([
+				"downloaded" => 1,
+			]);
+		}*/
 
 		$this->terminate();
 	}
