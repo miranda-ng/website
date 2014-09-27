58045215b39a767fecbacc8f1c0eb3ffec7a5ef4
Robyer <robyer@seznam.cz>
2014-09-27 16:10:35 +0200

    Fix plugin redirect

--- app/FrontModule/presenters/RedirectPresenter.php
+++ app/FrontModule/presenters/RedirectPresenter.php
@@ -1,13 +1,13 @@
 <?php
 
-use Nette\Http\IResponse;
-
 namespace FrontModule;
+
+use Nette\Http\IResponse;
 
 final class RedirectPresenter extends BasePresenter
 {
 	public function actionPlugin($id) {
-		$url = Macros::getWikiLink('Plugin:' . $id, $this->lang);
+		$url = \Macros::getWikiLink('Plugin:' . $id, $this->lang);
 
 		$httpResponse = $this->context->httpResponse;
 		$httpResponse->setCode(IResponse::S301_MOVED_PERMANENTLY);
