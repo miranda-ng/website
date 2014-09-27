509c70385fd09dd35919cb5a93665c42b363e4cd
Robyer <robyer@seznam.cz>
2014-09-27 15:19:51 +0200

    Fix maintenance file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Uncomment this line if you must temporarily take down your site for maintenance.
-// require '.maintenance.php';
+// require 'maintenance.html'; die();
 
 // Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/app/bootstrap.php';
