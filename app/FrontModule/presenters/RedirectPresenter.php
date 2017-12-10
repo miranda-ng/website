1878081cd5475bfc688e6ecd9cef31ffd6e92361
Robert Pösel <robyer@seznam.cz>
2017-12-10 12:50:16 +0100

    Improve redirect to wiki
    Perhaps to have same language as in browser (I don't remember anymore)

--- app/FrontModule/presenters/RedirectPresenter.php
+++ app/FrontModule/presenters/RedirectPresenter.php
@@ -10,6 +10,7 @@ final class RedirectPresenter extends BasePresenter
 		$url = \Macros::getWikiLink('Plugin:' . $id, $this->lang);
 
 		$httpResponse = $this->context->httpResponse;
+		$httpResponse->setHeader('Link', '<' . $url . '>; rel="alternate"; hreflang="x-default"');
 		$httpResponse->setCode(IResponse::S301_MOVED_PERMANENTLY);
 		$httpResponse->redirect($url);
 		exit;
